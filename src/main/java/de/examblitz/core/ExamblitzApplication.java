a90d98b3d1889faf9012b95dbd59570b97dd13e2
peu77 <emilebert@icloud.com>
2022-10-18 17:19:28 +0200

    first commit

---
+++ src/main/java/de/examblitz/core/ExamblitzApplication.java
@@ -0,0 +1,13 @@
+package de.examblitz.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamblitzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamblitzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/examblitz/core/ExamblitzApplicationTests.java
@@ -0,0 +1,13 @@
+package de.examblitz.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamblitzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
