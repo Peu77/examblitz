7d091024ea8d414b8dc20c9685c54c29f39f0807
BergerAPI <bergerbtw@gmail.com>
2022-10-19 20:54:46 +0200

    chore: Removed deprecated WebSecurityConfigurerAdapter

--- src/main/java/de/examblitz/core/config/SecurityConfig.java
+++ src/main/java/de/examblitz/core/config/SecurityConfig.java
@@ -2,17 +2,19 @@ package de.examblitz.core.config;
 
 import de.examblitz.core.auth.JwtAuthEntryPoint;
 import de.examblitz.core.auth.JwtRequestFilter;
+import de.examblitz.core.services.PrincipalService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
@@ -20,7 +22,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @AllArgsConstructor
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     private JwtAuthEntryPoint jwtAuthEntryPoint;
     private JwtRequestFilter jwtRequestFilter;
@@ -31,19 +33,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
+    public AuthenticationManager authManager(HttpSecurity http,
+                                             PasswordEncoder passwordEncoder,
+                                             PrincipalService principalService) throws Exception {
+
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                   .userDetailsService(principalService)
+                   .passwordEncoder(passwordEncoder)
+                   .and()
+                   .build();
     }
 
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable()
-                .authorizeRequests().antMatchers("/auth/public/**").permitAll().
-                anyRequest().authenticated().and().
-                exceptionHandling().authenticationEntryPoint(jwtAuthEntryPoint).
-                and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                    .authorizeRequests().antMatchers("/auth/public/**").permitAll().
+                    anyRequest().authenticated().and().
+                    exceptionHandling().authenticationEntryPoint(jwtAuthEntryPoint).
+                    and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return httpSecurity.build();
     }
 }
