be0caa934d836a1254c49dc4d86ff6b8825a7f49
BergerAPI <bergerbtw@gmail.com>
2022-10-23 14:17:54 +0200

    feat: test-repository

---
+++ src/main/java/de/examblitz/core/repository/TestRepository.java
@@ -0,0 +1,10 @@
+package de.examblitz.core.repository;
+
+import de.examblitz.core.model.TestModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TestRepository extends JpaRepository<TestModel, String> {
+
+
+
+}
